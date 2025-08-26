f24649de62c9c3c1ec7ce9d87ff5c349ac9f51e1
Amit Mahadik <amit.mahadik@gmail.com>
2025-08-25 18:57:51 -0700

    Initial Commit

---
+++ Okta MFA/Okta_MFAApp.swift
@@ -0,0 +1,17 @@
+//
+//  Okta_MFAApp.swift
+//  Okta MFA
+//
+//  Created by Mahadik, Amit on 8/25/25.
+//
+
+import SwiftUI
+
+@main
+struct Okta_MFAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
